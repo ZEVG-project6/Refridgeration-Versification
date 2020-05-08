5f776195da8b0d3b87345506aa0ec1eabf44dfb6
Zarina M <zarinabliss@gmail.com>
2020-05-08 17:20:10 -0400

    Updates

---
+++ precache-manifest.13627fa5d74143653ce19e24e1dcb3e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66a26a492b517fca71262562bd61c8d",
+    "url": "/Refridgeration-Versification/index.html"
+  },
+  {
+    "revision": "d0989a02eef8553ec91c",
+    "url": "/Refridgeration-Versification/static/css/main.ebb0bd58.chunk.css"
+  },
+  {
+    "revision": "06536d8b01243803e76e",
+    "url": "/Refridgeration-Versification/static/js/2.729b8ee0.chunk.js"
+  },
+  {
+    "revision": "a8382be8c3f6b499838384d253e88a50",
+    "url": "/Refridgeration-Versification/static/js/2.729b8ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0989a02eef8553ec91c",
+    "url": "/Refridgeration-Versification/static/js/main.7d494b3b.chunk.js"
+  },
+  {
+    "revision": "a3018cd1db7a2d3d92a3",
+    "url": "/Refridgeration-Versification/static/js/runtime-main.929e7ae7.js"
+  }
+]);
